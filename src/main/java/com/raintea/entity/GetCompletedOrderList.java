583ed3ff88f1fb918b780b4117283af0a52495ab
raintea <370465054@qq.com>
2018-04-20 17:55:35 +0800

    查询未分配订单(完成)---完善

---
+++ src/main/java/com/raintea/entity/GetCompletedOrderList.java
@@ -0,0 +1,22 @@
+package com.raintea.entity;
+
+
+public class GetCompletedOrderList {
+    private int order_id;
+    private int order_number;
+    private int community_id;
+    private String owner_name;
+    private String owner_phone;
+    private int worker_id;
+    private String address;
+    private String cause;
+    private String complete_time;
+    private String appointment_time;
+    private String progress;
+    private String repair_time;
+    private String isComment;
+
+
+
+
+}
